e7ab0c837dffa7011eb624c26ba5a2cde304c1b7
hondaminori <hondaminori.ta3@gmail.com>
2025-07-17 07:03:41 +0000

    PCからcommitとpush

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("こんにちは！世界");
+        System.out.println("こんにちは！世界２");
     }
 }
